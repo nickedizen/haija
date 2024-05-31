3bc68a47194eddf515a53999d0b324882e7ebc84
rickoandreaskartono <rickoandreaskartono_2226250037@mhs.mdp.ac.id>
2024-05-31 11:25:35 +0700

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:final_project_haijo/screens/categories_screen.dart';
-import 'package:final_project_haijo/screens/editProfile_screen.dart';
 import 'package:final_project_haijo/screens/favorites_screen.dart';
 import 'package:final_project_haijo/screens/main_screen.dart';
 import 'package:final_project_haijo/screens/profile_screen.dart';
 import 'package:final_project_haijo/screens/sign_in_screen.dart';
 import 'package:final_project_haijo/screens/sign_up_screen.dart';
 import 'package:final_project_haijo/screens/splash_screen.dart';
-import 'package:final_project_haijo/screens/userlain_screen.dart';
 import 'package:final_project_haijo/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -25,7 +23,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Haijo Library',
       theme: ThemeData(),
-      home:   UserLainScreen(),
+      home: ProfileScreen(),
       routes: {
         // '/homescreen': (context) => const HomeScreen(),
         '/signin': (context) => SignInScreen(),
